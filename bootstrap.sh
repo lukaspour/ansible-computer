50b91ad5a67e53696413d8f096a28761f8284311
Lukas Pour <lukaspour@gmail.com>
2017-09-01 21:02:48 +0200

    start

---
+++ bootstrap.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+unamestr=$(uname)
+
+# debian, ubuntu, mint etc.
+if [[ $unamestr == "Linux"  && -f $(which apt-get) ]]; then
+    sudo apt-get install -y software-properties-common
+    sudo apt-add-repository -y ppa:ansible/ansible
+    sudo apt-get update --yes
+    sudo apt-get install --yes python-pip python-dev git ansible
+fi
+
+sudo ansible-playbook setup.yml -i localhost, -c local
