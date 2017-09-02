b714e3a8ee2080b32eab85d3e390e9e3ee8781e8
Lukas Pour <lukaspour@gmail.com>
2017-09-02 11:27:43 +0200

    mate+insync

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,4 +10,4 @@ if [[ $unamestr == "Linux"  && -f $(which apt-get) ]]; then
     sudo apt-get install --yes git ansible
 fi
 
-sudo ansible-playbook setup.yml -i localhost, -c local -vv
+sudo ansible-playbook setup.yml -i localhost, -c local -vv --tags "$1"
