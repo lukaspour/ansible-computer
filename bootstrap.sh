f29cf4deff360dc347cb539fc187fef41ff9071a
Lukas Pour <lukaspour@gmail.com>
2017-11-12 13:36:58 +0100

    mate fixes

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,7 +7,7 @@ if [[ $unamestr == "Linux"  && -f $(which apt-get) ]]; then
     sudo apt-get install -y software-properties-common
     sudo apt-add-repository -y ppa:ansible/ansible
     sudo apt-get update --yes
-    sudo apt-get install --yes git ansible
+    sudo apt-get install --yes git ansible python-jmespath
 fi
 
 sudo ansible-playbook setup.yml -i localhost, -c local -vv --tags "$1"
