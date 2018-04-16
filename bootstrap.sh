803b0f3128dfb5400f717f89f054256fbfb8d1c8
Lukas Pour <lukaspour@gmail.com>
2018-04-16 10:11:58 +0200

    bionic

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,7 +9,7 @@ unamestr=$(uname)
 # debian, ubuntu, mint etc.
 if [[ $unamestr == "Linux"  && -f $(which apt-get) ]]; then
     sudo apt-get install -y software-properties-common
-    sudo apt-add-repository -y ppa:ansible/ansible
+#    sudo apt-add-repository -y ppa:ansible/ansible
     sudo apt-get update --yes
     sudo apt-get install --yes git ansible python-jmespath
 fi
