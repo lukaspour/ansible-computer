03fbf6de17b1281fbd1009343bafad967b776998
Lukas Pour <lukaspour@gmail.com>
2021-05-18 01:21:14 +0200

    fix

--- bootstrap.sh
+++ bootstrap.sh
@@ -18,7 +18,7 @@ if brew list ; then
     echo "brew command succeeded, skipping"
 else
     echo "brew command failed, configuring"
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
+    bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
     test -d ~/.linuxbrew && eval $(~/.linuxbrew/bin/brew shellenv)
     test -d /home/linuxbrew/.linuxbrew && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
     test -r ~/.bashrc && echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.bash_profile
